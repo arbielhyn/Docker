a58f7e220011dfeca39da69143093e4924f701a3
arbielhyn <150289854+arbielhyn@users.noreply.github.com>
2024-06-04 11:38:28 -0500

    Idk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
-import './App.css';
->>>>>>> a36eb61763bb8d00aa4d28bc22016fbca4610734
+import "./App.css";
+// a36eb61763bb8d00aa4d28bc22016fbca4610734
 
 function App() {
   return (
